7c03edfa7df5bd756f17e4ce3d50a7ccedd5927d
alexandrucancescu <anduhd@gmail.com>
2019-12-01 22:31:05 +0200

    added Frame interface properties

--- Common.d.ts
+++ Common.d.ts
@@ -22,12 +22,84 @@ export interface Packet {
 }
 
 export interface Frame {
+	type: string;
+	linesize: number;
+	width: number;
+	height: number;
+	data: Buffer[];
 	readonly flags: {
-		CORRUPT: boolean;
+		CORRUPT?: boolean;
+		DISCARD?: boolean;
 	};
+	nb_samples?: number;
+	format?: string;
+	key_frame?: boolean;
+	pict_type?: string;
+	sample_aspect_ratio?: number[];
+	pts?: number;
+	pkt_dts?: number;
+	coded_picture_number?: number;
+	display_picture_number?: number;
+	quality?: number;
+	repeat_pict?: number;
+	interlaced_frame?: boolean;
+	top_field_first?: boolean;
+	palette_has_changed?: boolean;
+	reordered_opaque?: any;
+	sample_rate?: number;
+	channel_layout?: string;
+	side_data?: any;
+	color_range?: string;
+	color_primaries?: string;
+	color_trc?: string;
+	colorspace?: string;
+	chroma_location?: string;
+	best_effort_timestamp?: number;
+	pkt_pos?: number;
+	pkt_duration?: number;
+	metadata?: object;
+	decode_error_flags?: object;
+	channels?: number;
+	pkt_size?: number;
+	crop_top?: number;
+	crop_bottom?: number;
+	crop_left?: number;
+	crop_right?: number;
+
+	/**
+	 * Beam coder exposes some of FFmpeg's ability to calculate the size of data buffers.
+	 * If you pass width, height and format properties for video frames, or channels/channel_layout,
+	 * sample_rate and format for audio frames, as options to the frame constructor then the linesize
+	 * array (number of bytes per line per plane) is computed. For video, multiply each value by the
+	 * height to get the minimum buffer size for the plane. For audio, the first element of the array
+	 * is the buffer size for each plane.
+	 *
+	 * To use the linesize numbers to automatically allocate buffers of the correct size,
+	 * call alloc() after the factory method
+	 */
+	alloc(): Frame;
 }
 
 /**
  * Packets for decoding can be created without reading them from a demuxer
  */
 export function packet(options: {pts: number, dts: number, data: Buffer}): Packet;
+
+/**
+ * Create a frame for encoding or filtering
+ */
+export function frame(options: {pts?: number, width: number, height: number, format: string, data?: Buffer[]}): Frame;
+
+/**
+ *  Details of the planes expected for each pixel format
+ */
+export function pix_fmts(): any[];
+
+export function sample_fmts(): any[];
+
+/**
+ * Note that when creating buffers from Javascript,
+ * FFmpeg recommends that a small amount of headroom is added to the minimum length of each buffer.
+ * The minimum amount of padding is exposed to Javascript as constant
+ */
+export const AV_INPUT_BUFFER_PADDING_SIZE: number;
