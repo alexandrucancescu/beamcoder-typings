a719f76c87fc4bd2c6b97f913f2c45706c0b896f
alexandrucancescu <anduhd@gmail.com>
2019-12-05 16:47:35 +0200

    added muxer

---
+++ Muxer.d.ts
@@ -0,0 +1,54 @@
+import { DemuxerStream } from "./Demuxer";
+
+export interface MuxerInfo {
+	type: "OutputFormat";
+	name: string;
+	long_name: string;
+	mime_type: string;
+	extensions: string;
+	audio_codec?: string;
+	video_codec?: string;
+	subtitle_codec?: string;
+	flags: {
+		[key: string]: any;
+	};
+	codec_tag: any[];
+	priv_class: object;
+}
+
+export interface MuxerInfoMap {
+	[key: string]: MuxerInfo;
+}
+
+export interface MuxerStream {
+	codecpar: {
+		height?: number;
+		width: number;
+		format: number; // Pixel format for video , sample format for audio
+		channels?: number,
+		sample_rate?: number,
+		channel_layout?: string,
+		block_align?: number, // Should be set for WAV
+		bits_per_coded_sample?: number,
+		bit_rate?: number;
+	};
+}
+
+export interface Muxer {
+	[key: string]: any;
+	newStream(options: {codec_name: string, timebase: number[], interleaved: boolean}): Promise<MuxerStream>;
+
+	/**
+	 * @returns On success, the returned promise resolves to undefined or,
+	 * if some of the options could not be set, an object containing an
+	 * unset property detailing which of the properties could not be set.
+	 */
+	openIO(options: {url: string, options: any}): Promise<null>;
+}
+
+export function muxers(): MuxerInfoMap;
+
+export function muxer(options: {filename: string}): Muxer;
+export function muxer(options: {format_name: string}): Muxer;
+export function muxer(options: {oformat: MuxerInfo}): Muxer;
+export function muxer(demuxerStream: DemuxerStream): Muxer;
--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,6 @@ export * from "./Demuxer";
 export * from "./Decoder";
 export * from "./Filters";
 export * from "./Encoder";
+export * from "./Muxer";
 
 export as namespace Beamcoder;
